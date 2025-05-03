2bdc0a299db2c066a562c6aba654df41a5bfc9b1
Kevin Schaerer <kevin.schaerer@nettimelogic.com>
2025-05-03 16:39:14 +0200

    fix

--- platform/ntl/hive-s/objects.mk
+++ platform/ntl/hive-s/objects.mk
@@ -29,15 +29,7 @@ PLATFORM_RISCV_CODE_MODEL = medany
 # Space separated list of object file names to be compiled for the platform
 platform-objs-y += platform.o
 
-#
-# If the platform support requires a builtin device tree file, the name of
-# the device tree compiled file should be specified here. The device tree
-# source file be in the form <dt file name>.dts
-#
-# platform-objs-y += <dt file name>.o
-
-# Optional parameter for path to external FDT
-# FW_FDT_PATH="path to platform flattened device tree file"
+FW_TEXT_START=0x40F00000
 
 FW_DYNAMIC=n
 
